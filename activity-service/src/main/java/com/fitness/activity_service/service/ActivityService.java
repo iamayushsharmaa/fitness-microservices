6fc2ff5360a6ae87774c80ed28e10ce4182e6377
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-10-08 19:51:16 +0530

    feat: add kafka producer and consumer in services

--- activity-service/src/main/java/com/fitness/activity_service/service/ActivityService.java
+++ activity-service/src/main/java/com/fitness/activity_service/service/ActivityService.java
@@ -5,6 +5,8 @@ import com.fitness.activity_service.dto.ActivityResponse;
 import com.fitness.activity_service.model.Activity;
 import com.fitness.activity_service.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +15,10 @@ public class ActivityService {
 
     private final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final KafkaTemplate<String, Activity> kafkaTemplate;
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
 
@@ -32,6 +38,13 @@ public class ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+
+        try {
+            kafkaTemplate.send(topicName, savedActivity.getUserId(), savedActivity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return mapToResponse(savedActivity);
     }
 
---
+++ ai-service/src/main/java/com/fitness/ai_service/service/ActivityMessageListener.java
@@ -0,0 +1,22 @@
+package com.fitness.ai_service.service;
+
+import com.fitness.ai_service.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+
+    private final ActivityAIService activityAIService;
+
+    @KafkaListener(topics = "${kafka.topic.name}", groupId = "activity-processor-group")
+    public void processActivity(Activity activity) {
+        log.info("Received activity for processing: {}", activity.getUserId());
+        activityAIService.generateRecommendation(activity);
+    }
+}
+
