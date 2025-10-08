bcd1acd9edfee37fc14184f7f24927f7c055f3fa
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-10-08 20:11:50 +0530

    feat: add extract methods to ai service

--- ai-service/src/main/java/com/fitness/ai_service/service/ActivityMessageListener.java
+++ ai-service/src/main/java/com/fitness/ai_service/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.ai_service.service;
 
 import com.fitness.ai_service.model.Activity;
+import com.fitness.ai_service.model.Recommendation;
+import com.fitness.ai_service.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,11 +14,13 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private final ActivityAIService activityAIService;
+    private final RecommendationRepository repository;
 
     @KafkaListener(topics = "${kafka.topic.name}", groupId = "activity-processor-group")
     public void processActivity(Activity activity) {
         log.info("Received activity for processing: {}", activity.getUserId());
-        activityAIService.generateRecommendation(activity);
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+        repository.save(recommendation);
     }
 }
 
