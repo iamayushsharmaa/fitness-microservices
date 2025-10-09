6f4cd01ccd6721d2f5e2254403a54c9e99b12c80
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-10-09 21:43:48 +0530

    feat: add security filter chain for oauth

---
+++ api-gateway/src/main/java/com/fitness/api_gateway/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.fitness.api_gateway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
