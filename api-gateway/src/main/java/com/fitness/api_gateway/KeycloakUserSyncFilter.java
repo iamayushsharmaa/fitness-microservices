d74bd1f372001c0c0be9243d35abd1a22cffc830
iamayushsharmaa <ayushsharmagit@gmail.com>
2025-10-09 23:25:12 +0530

    ref: refactor keycloak user sync filter class

--- api-gateway/src/main/java/com/fitness/api_gateway/KeycloakUserSyncFilter.java
+++ api-gateway/src/main/java/com/fitness/api_gateway/KeycloakUserSyncFilter.java
@@ -53,7 +53,7 @@ public class KeycloakUserSyncFilter implements WebFilter {
                         return chain.filter(exchange.mutate().request(mutatedRequest).build());
                     }));
         }
-        return null;
+        return chain.filter(exchange);
     }
 
     private RegisterRequest getUserDetails(String token) {
